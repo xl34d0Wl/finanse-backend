74ac4faef3b61d13148be4fab25b20f8456329ab
Jakub Lochowski <jakubelk@gmail.com>
2025-06-19 16:56:41 +0200

    23

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const { poolPromise } = require('./db');
 const app = express();
+const cors = require ('cors');
+app.use(cors());
 app.use(express.json());
 
 // Logowanie
